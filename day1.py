7e4ff80c33e90791a1b4c6095c4253bbaaa0267c
Ashish Ranjan <swolecoder@gmail.com>
2021-12-05 15:49:23 -0500

    day1

--- day1.py
+++ day1.py
@@ -1,22 +1,43 @@
-def readFile():
-    with open('./input.txt') as file:
+def readFile(textFile):
+    with open(textFile) as file:
         lines = file.readlines()
     return [int(line) for line in lines]
 
 
-
-def main():
-    data = readFile()
+# first part
+def first_part():
+    data = readFile('./firstFile.txt')
     count = 0
-    for i in range(1,len(data)):  
+    for i in range(1, len(data)):
         if data[i] > data[i-1]:
-            count +=1
+            count += 1
+
+    return count
+
+# second part
+
+
+def second_part():
+    data = readFile('./secondFile.txt')
+
+    windowSize = 3
+
+    runningSum = sum(data[:3])
+    index = 0
+    previousSum = runningSum
+    count = 0
+
+    for i in range(3, len(data)):
+        runningSum -= data[index]
+        runningSum += data[i]
+        if runningSum > previousSum:
+            count += 1
+
+        previousSum = runningSum
+        index += 1
 
     return count
 
 
-
-a = main()
+a = second_part()  # main()
 print(a)
-            
-        
